6b24a1a632cf88e5cbedacab0bfa194f406c1f41
Web-Martin <martin052017@outlook.com>
2024-03-20 09:50:46 -0400

    A issue in the upcoming day section has been fixed

--- index.js
+++ index.js
@@ -27,28 +27,22 @@ btnSearchWeather.addEventListener("click", async () => {
   async function nextDayWeather() {
     let lon = weatherData.coord.lon;
     let lat = weatherData.coord.lat;
-    let today = new Date();
-    const nextDay = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate() + 1
-    );
-    const formattedDate = nextDay.toISOString().slice(0, 10); // Format date as yyyy-mm-dd
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&q=${inptValue.value}&units=imperial& &appid=21c0cc44cd70f2c5f1ec8bd2f19c86ce`
-    );
 
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&q=${inptValue.value}&units=imperial&appid=21c0cc44cd70f2c5f1ec8bd2f19c86ce`
+    );
     const data = await response.json();
     return data;
   }
+
   const tomorrowWeather = await nextDayWeather();
-  console.log(tomorrowWeather.list[0]);
+  console.log(tomorrowWeather.list);
 
   const dayOne = document.getElementById("dayOne");
   const dayOneIcon = document.getElementById("dayOneIcon");
   const dayOneTemp = document.getElementById("dayOneTemp");
 
-  dayOne.innerText = tomorrowWeather.list[0].dt_txt;
+  dayOne.innerText = tomorrowWeather.list[7].dt_txt;
   dayOneIcon.src = `http://openweathermap.org/img/wn/${tomorrowWeather.list[0].weather[0].icon}.png`;
-  dayOneTemp.innerText = tomorrowWeather.list[0].main.temp;
+  dayOneTemp.innerText = `Temperature will be ${tomorrowWeather.list[7].main.temp} degree`;
 });
